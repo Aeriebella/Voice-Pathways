ac8dd20366aeff168eced3e623a248c41220af39
Aeriebella <Saerilia@gmail.com>
2026-02-24 14:26:59 -0500

    Add files via upload

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://voicepathways.com/sitemap.xml',
+    sitemap: 'https://www.voicepathways.com/sitemap.xml',
   }
 }
